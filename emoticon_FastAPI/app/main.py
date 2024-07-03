6e66ae7cb77d2e6e956010c2ecc4c419bdaf4b15
younghyen7956 <155882166+younghyen7956@users.noreply.github.com>
2024-07-03 10:48:20 +0900

    feat: [김용현] 기본 환경 구성 추가

--- emoticon_FastAPI/app/main.py
+++ emoticon_FastAPI/app/main.py
@@ -1,4 +1,11 @@
+import os
+
+import aiomysql
+from dotenv import load_dotenv
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+from async_db.database import getMySqlPool, createTableIfNeccessary
 
 app = FastAPI()
 
@@ -9,3 +16,38 @@ def read_root():
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: str = None):
     return {"item_id": item_id, "q": q}
+
+import warnings
+
+warnings.filterwarnings("ignore", category=aiomysql.Warning)
+
+async def lifespan(app: FastAPI):
+    # Startup
+    app.state.dbPool = await getMySqlPool()
+    await createTableIfNeccessary(app.state.dbPool)
+
+    yield
+
+    # Shutdown
+    app.state.dbPool.close()
+    await app.state.dbPool.wait_closed()
+
+
+load_dotenv()
+
+origins = os.getenv("ALLOWED_ORIGINS", "").split(",")
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="192.168.0.24", port=33333)
+    # 위에 각자 ip 주소로 변환해서 사용
+
